7ea9cfb67ee03e516859e260496c98f8136bc984
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-04-27 14:51:11 +0900

    Remove all files except README.md from main branch

--- src/main/java/com/taewoo/crudapplication/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.taewoo.crudapplication.repository;
-
-import com.taewoo.crudapplication.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
