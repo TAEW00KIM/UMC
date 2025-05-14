c33daa45fc199ad00f69f1e9cc7352e904bd35b7
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-05-14 14:28:29 +0900

    :sparkles: [FEAT] 특정 지역에 가게 추가 API

--- src/main/java/com/taewoo/study/repository/mission/MissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taewoo.study.repository.mission;
-
-import com.taewoo.study.domain.Mission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MissionRepository extends JpaRepository<Mission, Long>, MissionRepositoryCustom {
-}
