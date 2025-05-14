80b7bca8f4c25c4b7adb108c07e82b3c93e0c436
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-05-14 17:03:41 +0900

    :sparkles: [FEAT] 가게의 미션을 도전 중인 미션에 추가 API

--- src/main/java/com/taewoo/study/repository/regionRepository/RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taewoo.study.repository.regionRepository;
-
-import com.taewoo.study.domain.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionRepository extends JpaRepository<Region, Long> {
-}
