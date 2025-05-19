c2fceae37840ce751ef1cdfe2a074c0a7afed8aa
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-05-19 15:34:02 +0900

    Week9 on process

--- src/main/java/com/taewoo/study/web/controller/MemberRestController.java
+++ src/main/java/com/taewoo/study/web/controller/MemberRestController.java
@@ -2,26 +2,61 @@ package com.taewoo.study.web.controller;
 
 import com.taewoo.study.apiPayload.ApiResponse;
 import com.taewoo.study.converter.MemberConverter;
+import com.taewoo.study.converter.ReviewConverter;
 import com.taewoo.study.domain.Member;
+import com.taewoo.study.domain.Review;
 import com.taewoo.study.service.memberService.MemberCommandService;
+import com.taewoo.study.service.memberService.MemberQueryService;
+import com.taewoo.study.validation.annotation.ValidPage;
 import com.taewoo.study.web.dto.memberDto.MemberRequestDTO;
 import com.taewoo.study.web.dto.memberDto.MemberResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberRestController {
     private final MemberCommandService memberCommandService;
+    private final MemberQueryService memberQueryService;
 
     @PostMapping("/")
     public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDTO request) {
         Member member = memberCommandService.joinMember(request);
         return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+    }
+
+    @GetMapping("/{memberId}/reviews")
+    @Operation(summary = "내가 작성한 리뷰 목록 조회 API", description = "특정 회원이 작성한 리뷰들의 목록을 페이징하여 조회합니다. 페이지 번호는 1부터 시작합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "MEMBER4001",
+                    description = "사용자가 없습니다.",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "COMMON400",
+                    description = "잘못된 요청입니다.",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+    })
+    @Parameters({
+            @Parameter(name = "memberId", description = "회원 ID (Path Variable)", required = true),
+            @Parameter(name = "page", description = "페이지 번호 (Query String, 1부터 시작)", required = false)
+    })
+    public ApiResponse<MemberResponseDTO.MyReviewListDTO> getMyReviews(
+            @PathVariable Long memberId,
+            @ValidPage @RequestParam(name = "page", defaultValue = "1") Integer page
+    ) {
+        Integer pageZero = page - 1;
+        Page<Review> reviewPage = memberQueryService.getMyReviewList(memberId, pageZero);
+        return ApiResponse.onSuccess(ReviewConverter.toMyReviewListDTO(reviewPage));
     }
 }
--- src/main/java/com/taewoo/study/web/controller/StoreRestController.java
+++ src/main/java/com/taewoo/study/web/controller/StoreRestController.java
@@ -1,5 +1,7 @@
 package com.taewoo.study.web.controller;
 
+import com.taewoo.study.converter.ReviewConverter;
+import com.taewoo.study.domain.Review;
 import com.taewoo.study.service.storeService.StoreQueryService;
 import com.taewoo.study.validation.annotation.ExistStore;
 import com.taewoo.study.web.dto.reviewDto.ReviewResponseDTO;
@@ -19,6 +21,7 @@ import com.taewoo.study.web.dto.storeDto.StoreRequestDTO;
 import com.taewoo.study.web.dto.storeDto.StoreResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,6 +30,7 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "가게 API", description = "가게 등록/조회 등과 관련된 API입니다.")
 public class StoreRestController {
     private final StoreCommandService storeCommandService;
+    private final StoreQueryService storeQueryService;
 
     @PostMapping
     @Operation(summary = "가게 생성", description = "특정 지역에 가게를 새로 추가합니다.")
@@ -35,5 +39,34 @@ public class StoreRestController {
         Store store = storeCommandService.createStore(request);
         return ApiResponse.onSuccess(StoreConverter.toCreateResultDTO(store));
 
+    }
+
+    @GetMapping("/{storeId}/reviews")
+    @Operation(summary = "특정 가게의 리뷰 목록 조회 API", description = "특정 가게의 리뷰 목록을 조회하는 API이며 페이징을 포함합니다. query String으로 page 번호를 주세요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "COMMON200",
+                    description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "AUTH003",
+                    description = "access 토큰이 필요합니다.",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "AUTH004",
+                    description = "access 토큰 만료",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "AUTH006",
+                    description = "access 토큰에 이상 발생",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게의 ID, path variable")
+    })
+    public ApiResponse<ReviewResponseDTO.ReviewPreviewListDTO> getReviewList(
+            @ExistStore @PathVariable(name = "storeId") Long storeId,
+            @RequestParam(name = "page") Integer page) {
+        Page<Review> reviewList = storeQueryService.getReviewList(storeId, page);
+        return ApiResponse.onSuccess(ReviewConverter.reviewPreviewListDTO(reviewList));
     }
 }
