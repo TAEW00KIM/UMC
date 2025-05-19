d47975052d89b029ba0411c7fd7ae7dacedb6753
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-05-19 20:56:10 +0900

    :sparkles: [FEAT] 내가 도전 중인 미션 조회하기 API

--- src/main/java/com/taewoo/study/repository/memberMissionRepository/MemberMissionRepository.java
+++ src/main/java/com/taewoo/study/repository/memberMissionRepository/MemberMissionRepository.java
@@ -1,10 +1,23 @@
 package com.taewoo.study.repository.memberMissionRepository;
 
+import com.taewoo.study.domain.Member;
 import com.taewoo.study.domain.enums.MissionStatus;
 import com.taewoo.study.domain.mapping.MemberMission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface MemberMissionRepository extends JpaRepository<MemberMission, Long>, MemberMissionRepositoryCustom {
     boolean existsByMemberIdAndMissionIdAndStatus(Long memberId, Long missionId, MissionStatus status);
+
+    @Query("select mm from MemberMission mm " +
+            "join fetch mm.mission m " +
+            "join fetch m.store s " +
+            "where mm.member = :member and mm.status = :status")
+    Page<MemberMission> findAllByMemberAndStatusWithDetails(@Param("member")Member member,
+                                                            @Param("status") MissionStatus status,
+                                                            Pageable pageable);
 }
