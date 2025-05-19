3592d976f160f628bf54ea3f157d2e4584c0c64d
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-05-19 20:59:46 +0900

    Week9 on process

--- src/main/java/com/taewoo/study/service/memberMissionService/MemberMissionQueryService.java
+++ src/main/java/com/taewoo/study/service/memberMissionService/MemberMissionQueryService.java
@@ -1,10 +1,13 @@
 package com.taewoo.study.service.memberMissionService;
 
 import com.taewoo.study.domain.mapping.MemberMission;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface MemberMissionQueryService {
     List<MemberMission> findCompletedMissionByMember(Long memberId, Long cursorId, int limit);
     List<MemberMission> findChallengingMissionByMember(Long memberId, Long cursorId, int limit);
+
+    Page<MemberMission> getMyChallengingMissions(Long memberId, Integer page);
 }
--- src/main/java/com/taewoo/study/service/memberMissionService/MemberMissionQueryServiceImpl.java
+++ src/main/java/com/taewoo/study/service/memberMissionService/MemberMissionQueryServiceImpl.java
@@ -1,8 +1,17 @@
 package com.taewoo.study.service.memberMissionService;
 
+import com.taewoo.study.apiPayload.code.status.ErrorStatus;
+import com.taewoo.study.apiPayload.exception.handler.MemberHandler;
+import com.taewoo.study.domain.Member;
+import com.taewoo.study.domain.enums.MissionStatus;
 import com.taewoo.study.domain.mapping.MemberMission;
 import com.taewoo.study.repository.memberMissionRepository.MemberMissionRepository;
+import com.taewoo.study.repository.memberRepository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +22,7 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class MemberMissionQueryServiceImpl implements MemberMissionQueryService {
     private final MemberMissionRepository memberMissionRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     public List<MemberMission> findCompletedMissionByMember(Long memberId, Long cursorId, int limit) {
@@ -22,5 +32,14 @@ public class MemberMissionQueryServiceImpl implements MemberMissionQueryService 
     @Override
     public List<MemberMission> findChallengingMissionByMember(Long memberId, Long cursorId, int limit) {
         return memberMissionRepository.findChallengingMissionByMember(memberId, cursorId, limit);
+    }
+
+    @Override
+    public Page<MemberMission> getMyChallengingMissions(Long memberId, Integer page) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "createdAt"));
+        return memberMissionRepository.findAllByMemberAndStatusWithDetails(member, MissionStatus.CHALLENGING, pageable);
     }
 }
